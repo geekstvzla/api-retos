fbb1bc27d21ad3a0f7046f204d2b2cc4c6bd6b3a
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2026-01-18 23:20:35 -0400

    df

--- routes/users.js
+++ routes/users.js
@@ -102,7 +102,7 @@ router.get('/get-access-code', async function(req, res, next)
     var message = "";
     var status = "";
     var statusCode = 0;
-res.send(process.env.API_GEEKST);
+res.send({"p": "hola"});
 return;
     axios.get(process.env.API_GEEKST+'/users/get-access-code', { params: params})
     .then(async function (rs) {
